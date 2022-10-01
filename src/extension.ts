bc1c5cccbb75d8ce2cad3b6ed0a59c2c5203d9eb
jarupong pajakgo <lagmanmc@gmail.com>
2022-10-01 14:46:09 +0700

    refactor: es6 style (#1)
    * refactor: es6 style
    
    * style: fix typo
    
    Co-authored-by: Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,54 +1,78 @@
 import * as vscode from 'vscode';
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 
-const SalimAPIUrl: string = "https://watasalim.vercel.app/api/quotes"
-const AllSalimQuotes: string = "https://github.com/narze/awesome-salim-quotes/blob/main/README.md"
+const SALIM_API_URL = 'https://watasalim.vercel.app/api/quotes';
 
-const LinkToHeaven: string = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-const RickRollChance: number = 5
+const ALL_SALIM_QUOTE_API_URL =
+  'https://github.com/narze/awesome-salim-quotes/blob/main/README.md';
 
-let QuoteArray: string[] = []
+const LINK_TO_HEAVEN = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+
+const RICK_ROLL_CHANCE = 5;
+
+const quotesArray: string[] = [];
 
 export async function activate(context: vscode.ExtensionContext) {
+  // * Fetching Quotes from API
+  try {
+    const response = await fetch(SALIM_API_URL, {
+      method: 'GET',
+      headers: { 'Content-type': 'application/json;charset=UTF-8' },
+    });
 
-    // * Fetching Quotes from API
-    fetch(SalimAPIUrl, {
-        method: "GET",
-        headers: { "Content-type": "application/json;charset=UTF-8" }
-    }).then(response => response.json())
-        .then(json => {
-            for (let quote of json.quotes) {
-                QuoteArray.push(quote.body)
-            }
-            console.log("[EXTENSION'S QUOTE READY] Successfully pulled quote data from narze's repository")
-        })
-        .catch(err => console.log(err))
+    const json = await response.json();
 
-    let deliwry: vscode.Disposable = vscode.commands.registerCommand('salim-quote-delivery.getSalimQuote', () => {
-        // * Check for Errors
-        if (QuoteArray.length == 0) {
-            vscode.window.showErrorMessage("ERROR: Quote Array is empty. There maybe issues initializing or API is down.")
-            return
-        }
+    for (const quote of json.quotes) {
+      quotesArray.push(quote.body);
+    }
 
-        // * Random Quote Index
-        let randIndex: number = Math.floor(Math.random() * QuoteArray.length)
+    console.log(
+      "[EXTENSION'S QUOTE READY] Successfully pulled quote data from narze's repository"
+    );
+  } catch (error) {
+    console.error(error);
+  }
 
-        // * Show it
-        vscode.window.showInformationMessage(`${QuoteArray[randIndex]} (#${randIndex + 1})`);
+  const delivery: vscode.Disposable = vscode.commands.registerCommand(
+    'salim-quote-delivery.getSalimQuote',
+    () => {
+      // * Check for Errors
+      if (quotesArray.length === 0) {
+        vscode.window.showErrorMessage(
+          'ERROR: Quote Array is empty. There maybe issues initializing or API is down.'
+        );
+        return;
+      }
 
-        // * You have a Chance to be sent to Heaven
-        if (Math.floor(Math.random() * 100) < RickRollChance) {
-            vscode.env.openExternal(vscode.Uri.parse(LinkToHeaven))
-        }
-    })
+      // * Random Quote Index
+      const randIndex = Math.floor(
+        Math.random() * quotesArray.length
+      );
 
-    let showAllQuotes: vscode.Disposable = vscode.commands.registerCommand('salim-quote-delivery.showAllQuotes', () => {
-        vscode.env.openExternal(vscode.Uri.parse(AllSalimQuotes))
-    })
+      // * Show it
+      vscode.window.showInformationMessage(
+        `${quotesArray[randIndex]} (#${randIndex + 1})`
+      );
 
-    context.subscriptions.push(deliwry)
-    context.subscriptions.push(showAllQuotes)
+      // * You have a Chance to be sent to Heaven
+      if (Math.floor(Math.random() * 100) < RICK_ROLL_CHANCE) {
+        vscode.env.openExternal(vscode.Uri.parse(LINK_TO_HEAVEN));
+      }
+    }
+  );
+
+  const showAllQuotes: vscode.Disposable =
+    vscode.commands.registerCommand(
+      'salim-quote-delivery.showAllQuotes',
+      () => {
+        vscode.env.openExternal(
+          vscode.Uri.parse(ALL_SALIM_QUOTE_API_URL)
+        );
+      }
+    );
+
+  context.subscriptions.push(delivery);
+  context.subscriptions.push(showAllQuotes);
 }
 
-export function deactivate() { }
+export function deactivate() {}
